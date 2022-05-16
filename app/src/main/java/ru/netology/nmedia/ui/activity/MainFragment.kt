d6087798bede0302463af26d63a49f9038e45c04
NotCapengeR <purple.ka4ok@gmail.com>
2022-05-17 01:46:20 +0500

    Homework 3 have done!

--- app/src/main/java/ru/netology/nmedia/ui/activity/MainFragment.kt
+++ app/src/main/java/ru/netology/nmedia/ui/activity/MainFragment.kt
@@ -4,23 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentMainBinding
-import ru.netology.nmedia.ui.adapter.PostAdapter
-import ru.netology.nmedia.ui.adapter.PostListener
-import ru.netology.nmedia.ui.decorators.LinearVerticalSpacingDecoration
-import ru.netology.nmedia.ui.viewmodel.PostViewModel
-import ru.netology.nmedia.utils.AndroidUtils
-import timber.log.Timber
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: PostViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,50 +24,7 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = PostAdapter(object : PostListener {
-            override fun onAdded(title: String, text: String): Long {
-                return viewModel.addPost(title, text)
-            }
-
-            override fun onRemoved(id: Long): Boolean {
-                Timber.d("Removed post with id $id")
-                return viewModel.removePost(id)
-            }
-
-            override fun onEdit(id: Long, newText: String): Boolean {
-                return viewModel.editPost(id, newText)
-            }
-
-            override fun onLiked(id: Long): Boolean {
-                return viewModel.likePost(id)
-            }
-
-            override fun onShared(id: Long): Int {
-                return viewModel.sharePost(id)
-            }
-
-            override fun onCommented(id: Long): Int {
-                return viewModel.commentPost(id)
-            }
-
-            override fun onPostMoved(id: Long, movedBy: Int): Long {
-                return viewModel.movePost(id, movedBy)
-            }
-        })
-        adapter.setHasStableIds(true)
-        with(binding) {
-            rcViewPost.layoutManager =
-                LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
-            rcViewPost.adapter = adapter
-            rcViewPost.addItemDecoration(
-                LinearVerticalSpacingDecoration(
-                    AndroidUtils.dpToPx(activity as AppCompatActivity, 5)
-                )
-            )
-        }
-        viewModel.postsList.observe(activity as AppCompatActivity) {
-            adapter.submitList(it)
-        }
+        binding.ivPostAvatar.setImageResource(R.drawable.ic_launcher_foreground)
     }
 
 
