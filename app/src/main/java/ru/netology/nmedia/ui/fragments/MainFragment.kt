7cb519f6f56bce1899176febb7f27e96b6720744
capenger <capacitor2004@mail.ru>
2022-06-25 18:44:48 +0300

    Network + Swipe to refresh

--- app/src/main/java/ru/netology/nmedia/ui/fragments/MainFragment.kt
+++ app/src/main/java/ru/netology/nmedia/ui/fragments/MainFragment.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
