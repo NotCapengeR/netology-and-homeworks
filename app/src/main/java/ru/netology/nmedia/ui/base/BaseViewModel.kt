0a2a4ecc30d8b8e78f0018f217adb5cbc5e4bfaa
capenger <capacitor2004@mail.ru>
2022-07-10 16:15:44 +0300

    Small refactoring

--- app/src/main/java/ru/netology/nmedia/ui/base/BaseViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/ui/base/BaseViewModel.kt
@@ -1,20 +1,15 @@
 package ru.netology.nmedia.ui.base
 
 import android.app.Application
-import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.lifecycle.AndroidViewModel
 import ru.netology.nmedia.App
+import ru.netology.nmedia.utils.AndroidUtils
 
 abstract class BaseViewModel(application: Application) : AndroidViewModel(application) {
 
     protected fun showToast(message: String?, isLong: Boolean = false) {
-        if (message == null) return
-        if (isLong) {
-            Toast.makeText(this.getApplication(), message, Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(this.getApplication(), message, Toast.LENGTH_SHORT).show()
-        }
+        AndroidUtils.showToast(this.getApplication(), message, isLong)
     }
 
     protected fun showToast(@StringRes msgResId: Int, isLong: Boolean = false) {
